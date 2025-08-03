81ddcbec0f37d1e113ec7cde3e049b6effb5bbb1
Ore Victor <adesanya.ov@gmail.com>
2025-08-03 21:20:35 +0100

    script to insert a single row in a newly created Customer table (database alx_book_store) in your MySQL server

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,39 @@
-USE alx_book_store;
+import mysql.connector
 
-INSERT INTO Customer (customer_id, customer_name, email, address)
-VALUES ('1', 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+-- Initialization: initialize connection to MySQL server
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Ore@sanya1911",
+    database="alx_book_store"
+)
+
+-- Initialiazation: cursor created to execute sql commands
+cursor = connection.cursor()
+
+-- Command to create Customer table
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Customer (
+        customer_id INT AUTO_INCREMENT PRIMARY KEY,
+        customer_name VARCHAR(215) NOT NULL,
+        email VARCHAR(215),
+        address TEXT
+    );
+""")
+
+-- Command to insert a single customer record 
+cursor.execute("""
+    INSERT INTO Customer (customer_id, customer_name, email, address)
+    VALUES (%s, %s, %s, %s)
+""", (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'))
+
+-- Command to commit the changes
+connection.commit()
+
+print("Table 'Customer' created successfully.")
+
+-- Command to close the connection
+cursor.close()
+connection.close()
 
 
