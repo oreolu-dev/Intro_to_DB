d348cbbcd4df7c8809f1878777af4f232ac25162
Ore Victor <adesanya.ov@gmail.com>
2025-08-03 22:09:02 +0100

    script to print the full description of the table books from the database alx_book_store in MySQL server

--- task_4.sql
+++ task_4.sql
@@ -1,26 +1,4 @@
-import mysql.connector;
-
--- Initialization: initialize connection to MySQL server
-connection = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Ore@sanya1911",
-    database="alx_book_store"
-)
-
--- Initialiazation: cursor created to execute sql commands
-cursor = connection.cursor()
-
--- Command to create Customer table
-cursor.execute("""
-    CREATE TABLE books(
-        book_id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(130) NOT NULL,
-        author_id INT,
-        price DOUBLE NOT NULL,
-        publication_date DATE
-    );
-""")
+USE alx_book_store;
 
 SELECT 
     COLUMN_NAME,
@@ -32,7 +10,7 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_NAME = 'books'
+    TABLE_NAME = 'Books'
     AND TABLE_SCHEMA = 'alx_book_store';
 
 
